28122ef03b6b705243e5b8d3e10fc70f80982a53
root <psk221219@gmail.com>
2025-02-22 03:54:28 +0300

    added worker settings

--- src/config/configs.py
+++ src/config/configs.py
@@ -13,4 +13,12 @@ class DBSettings(BaseSetting):
     POSTGRES_PASSWORD: str
 
 
+class RabbitSettings(BaseSetting):
+    RABBIT_HOST: str
+    RABBIT_USER: str
+    RABBIT_PASSWORD: str
+    RABBIT_QUEUE: str
+
+
+rabbit_settings = RabbitSettings()
 db_settings = DBSettings()
