5a0da2793606fceef9b3f5c21130555c55601a2f
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2025-02-22 04:14:54 +0300

    Delete src/utilities/worker_url.py

--- src/utilities/worker_url.py
+++
@@ -1,5 +0,0 @@
-from config.configs import rabbit_settings
-
-
-def get_url() -> str:
-    return f"ampq://{rabbit_settings.RABBIT_USER}:{rabbit_settings.RABBIT_PASSWORD}@{rabbit_settings.RABBIT_HOST}/"  # noqa: E501
